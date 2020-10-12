39baee41fda69c373eece8ca0f061d16fbb8f9a3
Joshua Busacker <joshua@busacker.xyz>
2020-10-13 07:41:03 +1100

    Add website

--- website/frontend.py
+++ website/frontend.py
@@ -8,10 +8,19 @@ from website import utils as webutils
 routes = RouteTableDef()
 
 
+@routes.get("/discord")
+@template('discord.j2')
 @webutils.add_output_args()
+async def discord_code(request: Request):
     """Index of the website"""
 
+    return {"code": request.query.get('code')}
 
 
+@routes.get("/")
+@template('index.j2')
+@webutils.add_output_args()
+async def index(request: Request):
     """Index of the website"""
 
+    return {}
